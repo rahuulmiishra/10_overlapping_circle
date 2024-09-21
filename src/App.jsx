7825bf385e9ade967cfcc1fc3fc3da09e31762a5
Rahul Mishra <rahulmishra21791@gmail.com>
2024-09-21 10:11:18 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import OverlappingCircle from "./overlapping-circle";
+
+function App() {
+  return <OverlappingCircle />;
+}
+
+export default App;
